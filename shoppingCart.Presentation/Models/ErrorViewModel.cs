1a19f370484b45cf2331b4eca1516a5f38360192
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2024-06-02 02:57:58 +0300

    Update PresentaionLayer && AddRazorRuntimeCompilation

---
+++ shoppingCart.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shoppingCart.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
