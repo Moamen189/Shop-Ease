00c28a126b78d5b6e93f9b6e111df581754d1528
Moamen ASHRAF <moamen.ashraf@intalio.com>
2024-06-03 12:08:57 +0300

    Apply DataBase Configrations

---
+++ shoppingCart.Presentation/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shoppingCart.Presentation.Models;
+
+namespace shoppingCart.Presentation.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
