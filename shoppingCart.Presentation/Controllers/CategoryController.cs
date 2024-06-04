bfe0a82d6e8485d5b5aa9b99db42d737a1d4b766
Moamen ASHRAF <moamen.ashraf@intalio.com>
2024-06-04 11:46:26 +0300

    get , add category

---
+++ shoppingCart.Presentation/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingCart.Presentation.Data;
+using shoppingCart.Presentation.Models;
+
+namespace shoppingCart.Presentation.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = context.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create() { 
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
