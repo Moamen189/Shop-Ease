cd4ecddae6176e75ef1b022f4428be9779f6026b
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2024-06-08 21:41:59 +0300

    RepositoryImplementation

---
+++ shoppingCart.DataAcess/Impementation/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using shoppingCart.DataAcess.Data;
+using shoppingCart.Entities.Models;
+using shoppingCart.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppingCart.DataAcess.Impementation
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoryRepository(ApplicationDbContext context):base(context)
+        {
+            this.context = context;
+        }
+        public void Update(Category category)
+        {
+            var categoryInDb = context.Categories.FirstOrDefault(x=> x.Id == category.Id);
+
+            if (categoryInDb != null)
+            {
+                categoryInDb.Name = category.Name;
+                categoryInDb.Description = category.Description;
+                categoryInDb.CreatedTime = DateTime.Now;
+            }
+        }
+    }
+}
---
+++ shoppingCart.DataAcess/Impementation/GenericRepository.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shoppingCart.DataAcess.Data;
+using shoppingCart.Entities.Models;
+using shoppingCart.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppingCart.DataAcess.Impementation
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity); 
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate, string? IncludeWord)
+        {
+            IQueryable<T> query = context.Set<T>();
+
+            if(predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if(IncludeWord != null)
+            {
+                foreach(var item in IncludeWord.Split(new char[] {','} , StringSplitOptions.RemoveEmptyEntries))
+                {
+                           query = query.Include(item);    
+                }
+            }
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> predicate, string? IncludeWord)
+        {
+            IQueryable<T> query = context.Set<T>();
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (IncludeWord != null)
+            {
+                foreach (var item in IncludeWord.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);
+                }
+            }
+            return query.SingleOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            context.Set<T>().Remove(entity);
+
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            context.Set<T>().RemoveRange(entities);
+
+        }
+    }
+}
