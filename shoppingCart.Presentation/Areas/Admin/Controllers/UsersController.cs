63ab63a44e807d02ae01cf5d44e8595a8352c341
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2024-07-12 21:56:06 +0300

    UsersControllers

---
+++ shoppingCart.Presentation/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingCart.DataAcess.Data;
+using System.Security.Claims;
+
+namespace shoppingCart.Presentation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _Context;
+
+        public UsersController(ApplicationDbContext Context)
+        {
+            _Context = Context;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            string userId = claim.Value;
+            return View(_Context.ApplicationUsers.Where(x => x.Id != userId).ToList());
+        }
+    }
+}
