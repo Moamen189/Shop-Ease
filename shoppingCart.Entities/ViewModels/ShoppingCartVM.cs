0c0329e255f0384aee2c07b853c695c38bfecdf8
Moamen ASHRAF <moamen.ashraf@intalio.com>
2024-07-16 22:05:56 +0300

    Update Cart Validation

---
+++ shoppingCart.Entities/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,16 @@
+﻿using shoppingCart.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppingCart.Entities.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCartDetails> CartsList { get; set; }
+
+        public decimal TotalCarts { get; set; }
+    }
+}
--- shoppingCart.Presentation/Areas/Customer/Controllers/CartController.cs
+++ shoppingCart.Presentation/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using shoppingCart.Entities.Repositories;
+using shoppingCart.Entities.ViewModels;
 
 namespace shoppingCart.Presentation.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
+            var claimsIdentity = (System.Security.Claims.ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                CartsList = unitOfWork.ShoppingCartDetails.GetAll(x => x.ApplicationUserId == claim.Value , IncludeWord:"Product")
+            };
+            foreach(var list in ShoppingCartVM.CartsList)
+			{
+				ShoppingCartVM.TotalCarts += (list.Product.Price * list.Count);
+				
+			}
             return View();
         }
+
+        public IActionResult Plus(int cartId)
+		{
+			var cart = unitOfWork.ShoppingCartDetails.GetFirstOrDefault(x => x.Id == cartId, IncludeWord: "Product");
+			unitOfWork.ShoppingCartDetails.increaseCount(cart, 1);
+			//cart.Count += 1;
+			unitOfWork.Complete();
+			return RedirectToAction("Index");
+		}
+
+        public IActionResult Minus(int cartId)
+        {
+			var cart = unitOfWork.ShoppingCartDetails.GetFirstOrDefault(x => x.Id == cartId, IncludeWord: "Product");
+            if(cart.Count == 1)
+            {
+				unitOfWork.ShoppingCartDetails.Remove(cart);
+				unitOfWork.Complete();
+				var cnt = unitOfWork.ShoppingCartDetails.GetAll(x => x.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+				HttpContext.Session.SetInt32("CartCount", cnt);
+			}
+			else
+			{
+				//cart.Count -= 1;
+				unitOfWork.ShoppingCartDetails.decreaseCount(cart, 1);
+				unitOfWork.Complete();
+			}
+            return RedirectToAction("Index");
+		}
+
+		public IActionResult Remove(int cartId)
+		{
+			var cart = unitOfWork.ShoppingCartDetails.GetFirstOrDefault(x => x.Id == cartId, IncludeWord: "Product");
+			unitOfWork.ShoppingCartDetails.Remove(cart);
+			unitOfWork.Complete();
+			var cnt = unitOfWork.ShoppingCartDetails.GetAll(x => x.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+			HttpContext.Session.SetInt32("CartCount", cnt);
+			return RedirectToAction("Index");
+		}
     }
 }
