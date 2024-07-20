7d3ac92b772c7394d65eed384b98c23498891803
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2024-07-20 04:14:23 +0300

    update

--- shoppingCart.Entities/Models/OrderHeader.cs
+++ shoppingCart.Entities/Models/OrderHeader.cs
@@ -22,7 +22,7 @@ namespace shoppingCart.Entities.Models
 		public string? PaymentStatus { get; set; }
 		public string? TrackingNumber { get; set; }
 		public string? Carrier { get; set; }
-		public DateTime? PaymentDate { get; set; }
+		public DateTime PaymentDate { get; set; }
 		public string? SessionId { get; set; }
 		public string? PaymentIntentId { get; set; }
 
