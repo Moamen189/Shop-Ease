9136bb591f630f8b07fb41d05020c61a3183d218
Moamen ASHRAF <moamen.ashraf@intalio.com>
2024-07-23 10:19:55 +0300

    Dashboard Page for Admin

---
+++ shoppingCart.Presentation/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using shoppingCart.Entities.Repositories;
+using ShoppingCart.Utilities;
+
+namespace shoppingCart.Presentation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.AdminRole)]
+    public class DashboardController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Orders = unitOfWork.Order.GetAll().Count();
+            ViewBag.Products = unitOfWork.Product.GetAll().Count();
+            ViewBag.Users = unitOfWork.ApplicationUser.GetAll().Count();
+            ViewBag.ApprovedOrders = unitOfWork.Order.GetAll(x=> x.OrderStatus == SD.Approve).Count();
+            return View();
+        }
+    }
+}
